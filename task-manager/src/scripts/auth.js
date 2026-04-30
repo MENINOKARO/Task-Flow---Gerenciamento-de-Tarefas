70cbbf36eb5fc79826caf18cc12cceb8fee5385e
MatheusDev <119450919+LinkMatheusDev@users.noreply.github.com>
2026-04-29 21:37:40 -0300

    permanencia de usuario na pagina

--- task-manager/src/scripts/auth.js
+++
@@ -1,7 +0,0 @@
-const isLogged = localStorage.getItem("isLogged");
-
-if (!isLogged) {
-  window.location.href = "login.html";
-}
-
-//Controle de autentiação, se usuario não tiver logado ele voltara para pagina de login 
